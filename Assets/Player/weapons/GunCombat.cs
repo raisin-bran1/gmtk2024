40b3dd9fd8d6b6f5ad45359222984e63b5eb0626
BuildTools <unconfigured@null.spigotmc.org>
2024-08-19 15:54:00 -0700

    frikin guns

--- Assets/Player/weapons/GunCombat.cs
+++ Assets/Player/weapons/GunCombat.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GunCombat : MonoBehaviour
 {
 
-    public float fireGap, damage, fireSpeed;
+    public float fireGap, damage, fireSpeed, idleX, idleY, idleRot, firingX, firingY, firingRot;
+    public bool isPistol;
     public GameObject bullet;
 
     SpriteRenderer spriteRenderer;
@@ -25,7 +26,7 @@ public class GunCombat : MonoBehaviour
     public void Fire()
     {
         Quaternion rot = Quaternion.Euler(0, 0, -90);
-        GameObject newBullet = Instantiate(bullet, transform.position, rot);
+        GameObject newBullet = Instantiate(bullet, transform.position + new Vector3(0, 0.1f, 0), rot);
         newBullet.GetComponent<Projectile>().damage = damage;
         newBullet.GetComponent<Projectile>().speed *= fireSpeed;
         if (transform.localScale.x < 0)
@@ -33,6 +34,39 @@ public class GunCombat : MonoBehaviour
             rot = Quaternion.Euler(0, 0, 90);
             newBullet.GetComponent<Projectile>().speed *= -1;
         }
+    }
+
+    public void Position(float flip, bool firing)
+    {
+        if (firing)
+        {
+            transform.position = transform.parent.gameObject.transform.position + new Vector3(flip * firingX * Mathf.Abs(transform.parent.gameObject.transform.localScale.x), firingY * Mathf.Abs(transform.parent.gameObject.transform.localScale.y), 0);
+            transform.rotation = Quaternion.Euler(0, 0, firingRot * flip);
+        }
+        else
+        {
+            transform.position = transform.parent.gameObject.transform.position + new Vector3(flip * idleX * Mathf.Abs(transform.parent.gameObject.transform.localScale.x), idleY * Mathf.Abs(transform.parent.gameObject.transform.localScale.y), 0);
+            transform.rotation = Quaternion.Euler(0, 0, idleRot * flip);
+        }
+        Vector3 s = transform.localScale;
+        s.x = flip * Mathf.Abs(s.x);
+        transform.localScale = s;
+    }
+
+    public IEnumerator Wiggle()
+    {
+        Vector3 pos = transform.position;
+        int flip = 1;
+        if (transform.localScale.x < 0)
+        {
+            flip = -1;
+        }
+        pos.x -= 0.1f * flip;
+        transform.position = pos;
+        yield return new WaitForSeconds(fireGap / 2);
+        pos = transform.position;
+        pos.x += 0.1f * flip;
+        transform.position = pos;
     }
 
 }
